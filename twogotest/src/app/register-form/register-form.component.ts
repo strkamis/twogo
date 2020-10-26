60dc2dbf4532d9e5c3574accfc7970a62421755e
Kamila Bezerra da Silva <kamilabezerra@brq.com>
2020-10-26 03:42:56 -0300

    colocando validações e arrumando problemas no consumo da api

--- twogotest/src/app/register-form/register-form.component.ts
+++ twogotest/src/app/register-form/register-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Validacoes } from '../shared/models/validacoes';
+import { User } from '../shared/models/usuarios';
+import { AuthService } from '../shared/services/auth-service';
+import { ApiService } from '../shared/services/api.service';
 
 @Component({
   selector: 'app-register-form',
@@ -7,9 +12,146 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterFormComponent implements OnInit {
 
-  constructor() { }
+  FormRegister: FormGroup;
+  IsRegistered: User;
+  status = {
+    isDanger: true,
+    valid: true };
+  MsgError = '';
+
+  constructor(private fb: FormBuilder,
+              public authService: AuthService,
+              public API: ApiService) { }
 
   ngOnInit(): void {
+    this.newRegisterForm();
+    this.status.valid = true;
+    this.authService.status.valid = true;
+  }
+  NewRegister() {
+    this.status.valid = true;
+
+    if (this.FormRegister.valid) {
+      const FormData = this.FormRegister.value;
+      const registered = this.userExists(FormData.email);
+      if (!registered) {
+
+      const user = new User(
+        FormData.name,
+        FormData.email,
+        FormData.cpf,
+        FormData.cnpj,
+        FormData.phone,
+        FormData.namecomercial,
+        // false
+      );
+
+      this.authService.SignUp(FormData.email, FormData.password);
+      this.API.createUser(user).subscribe(
+        res => { console.log('Novo usuário add com sucesso'); },
+        err => { console.log('falha:' + err); }
+        );
+      } else {
+        this.status.isDanger = true;
+        this.status.valid = false;
+        this.MsgError = 'Usuário já está registrado!';
+      }
+    } else {
+      this.status.isDanger = true;
+      this.status.valid = false;
+      this.MsgError = 'Verifique os campos do seu formulário!';
+    }
+    // console.log(`O usuário ${user.name} foi cadastrado com sucesso. \n Dados: ${JSON.stringify(user)}`);
+  }
+
+  userExists(email: string): boolean {
+    this.IsRegistered = undefined;
+    this.API.getUser(email).subscribe((data) => {
+      this.IsRegistered = data;
+      console.log(data);
+    });
+    return (this.IsRegistered !== undefined);
+
+  }
+  newRegisterForm() {
+    this.FormRegister = this.fb.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(100)
+        ])
+      ],
+      email: [
+        '',
+        Validators.compose([
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'),
+          Validators.required
+        ])
+      ],
+      cpf: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validacoes.ValidaCpf,
+          Validators.maxLength(11)])
+      ],
+      cnpj: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[0-9]{9}')
+        ])
+      ],
+      phone: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[0-9]{5,11}' )
+        ])
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12)
+        ])
+      ],
+      confirmPassword: ['', Validators.compose([Validators.required])]
+    },
+    {
+      validator: Validacoes.CombinePwd
+    }
+    );
+  }
+
+  get name() {
+    return this.FormRegister.get('name');
+  }
+  get email() {
+    return this.FormRegister.get('email');
+  }
+  get cpf() {
+    return this.FormRegister.get('cpf');
+  }
+  get cnpj() {
+    return this.FormRegister.get('cnpj');
+  }
+  get phone() {
+    return this.FormRegister.get('phone');
+  }
+  get password() {
+    return this.FormRegister.get('password');
+  }
+  get confirmPassword() {
+    return this.FormRegister.get('confirmPassword');
+  }
+  get nameComercial() {
+    return this.FormRegister.get('namecomercial');
   }
 
 }
+
+
--- twogotest/src/environments/environment.ts
+++ twogotest/src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA1YeYMXxQ9VDEKDT_oeFRwd67Y8rvNoCs",
+    authDomain: "twogo-62a22.firebaseapp.com",
+    databaseURL: "https://twogo-62a22.firebaseio.com",
+    projectId: "twogo-62a22",
+    storageBucket: "twogo-62a22.appspot.com",
+    messagingSenderId: "1096934581175",
+    appId: "1:1096934581175:web:1c2bf83ba788c452253e32",
+    measurementId: "G-WBKRHV45D1"
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -13,4 +22,6 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  
+/* Included with Angular CLI*/
+}
